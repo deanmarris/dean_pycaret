c2552dcd216a72cb473a90d7bab853c6d840c1b4
Dean Marris <dean@marris.me>
2022-12-04 20:54:41 -0700

    my initial commit

---
+++ src/data/data_prep.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import os
+import sys
+import time
+import datetime
+import pickle
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+
+df = pd.DataFrame({'A': [1,2,3], 'B': [4,5,6], 'C':[7,8,9]})
+df
+
